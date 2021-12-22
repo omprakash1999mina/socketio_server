eff434cedc6fd4083dd292dbee0ddc2d2b40509a
OP_Developer <op1999mina@gmail.com>
2021-12-22 17:36:01 +0530

    done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // const io = require('socket.io')(3000)
 console.log('server is rununning at port : 5000')
 
-const io = require('socket.io')(5000, {
+const io = require('socket.io')(process.env.PORT || 5000, {
   cors: {
     origin: ['http://localhost:3000']
   }
